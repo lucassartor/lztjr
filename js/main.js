7fc04fef5cb2d3ebb6e7472e7f7212651b0e5c75
LucasMS115 <57019723+LucasMS115@users.noreply.github.com>
2020-05-24 17:08:41 -0300

    Section "O que falam de nos"
    O que falam de nos V1.

--- js/main.js
+++ js/main.js
@@ -71,25 +71,27 @@ AOS.init({
         });
 
         $('.carousel-testimony').owlCarousel({
-            center: true,
-            loop: true,
-            items:1,
-            margin: 30,
-            stagePadding: 0,
-            nav: true,
-            navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-            responsive:{
-                0:{
-                    items: 1
-                },
-                600:{
-                    items: 2
-                },
-                1000:{
-                    items: 3
-                }
-            }
-        });
+			center: true,
+			loop: true,
+			autoplay: true,
+			autoplaySpeed:2000,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
     };
     carousel();
 
