4c1b5a22698482a9b1a69caf09090df041f2f6dc
LucasMS115 <57019723+LucasMS115@users.noreply.github.com>
2020-06-06 19:26:32 -0300

    Add carousel portifolio v1

--- js/main.js
+++ js/main.js
@@ -91,7 +91,31 @@ AOS.init({
 					items: 3
 				}
 			}
-		});
+        });
+        
+        $('.carousel-portifolio').owlCarousel({
+			center: true,
+			loop: true,
+			autoplay: true,
+			autoplaySpeed:2000,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+                },
+			}
+        });
+        
     };
     carousel();
 
